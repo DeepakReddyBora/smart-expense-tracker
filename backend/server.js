2e53180c1167b458c991ec9579ddc6ba1960db5e
Deepak <boradeepakreddy2005@gmail.com>
2026-03-03 18:03:58 +0530

    Update

--- backend/server.js
+++ backend/server.js
@@ -16,7 +16,10 @@ const app = express();
 
 app.use(cors());
 
-
+app.use(cors({
+  origin: "https://smart-expense-tracker-chi.vercel.app", 
+  credentials: true
+}));
 app.use(express.json());
 
 app.use("/api/auth", authRoutes);
