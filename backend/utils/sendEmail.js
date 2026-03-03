790c85595b7de3a3fdc4d253a7606f135071e109
Deepak <boradeepakreddy2005@gmail.com>
2026-03-03 18:39:06 +0530

    Fixed

--- backend/utils/sendEmail.js
+++ backend/utils/sendEmail.js
@@ -3,12 +3,18 @@ import { Resend } from "resend";
 const resend = new Resend(process.env.RESEND_API_KEY);
 
 const sendEmail = async (to, subject, text) => {
-  await resend.emails.send({
-    from: "onboarding@resend.dev",
-    to,
-    subject,
-    text,
-  });
+  try {
+    const response = await resend.emails.send({
+      from: "onboarding@resend.dev",
+      to,
+      subject,
+      text,
+    });
+
+    console.log("Email sent", response);
+  } catch(error) {
+    console.log("Email not sent", error);
+  }
 };
 
 
