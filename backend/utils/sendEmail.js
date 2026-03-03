d7631fce305f0b77338521703174b27cb4a32ffd
Deepak <boradeepakreddy2005@gmail.com>
2026-03-03 21:15:09 +0530

    Gmail OAuth configured

--- backend/utils/sendEmail.js
+++ backend/utils/sendEmail.js
@@ -1,21 +1,45 @@
-import { Resend } from "resend";
+import nodemailer from "nodemailer";
+import { google } from "googleapis";
 
-const resend = new Resend(process.env.RESEND_API_KEY);
+const OAuth2 = google.auth.OAuth2;
 
 const sendEmail = async (to, subject, text) => {
   try {
-    const response = await resend.emails.send({
-      from: "onboarding@resend.dev",
+    const oauth2Client = new OAuth2(
+      process.env.CLIENT_ID,
+      process.env.CLIENT_SECRET,
+      "https://developers.google.com/oauthplayground"
+    );
+
+    oauth2Client.setCredentials({
+      refresh_token: process.env.REFRESH_TOKEN,
+    });
+
+    const accessToken = await oauth2Client.getAccessToken();
+
+    const transporter = nodemailer.createTransport({
+      service: "gmail",
+      auth: {
+        type: "OAuth2",
+        user: process.env.EMAIL,
+        clientId: process.env.CLIENT_ID,
+        clientSecret: process.env.CLIENT_SECRET,
+        refreshToken: process.env.REFRESH_TOKEN,
+        accessToken: accessToken.token,
+      },
+    });
+
+    await transporter.sendMail({
+      from: process.env.EMAIL,
       to,
       subject,
       text,
     });
 
-    console.log("Email sent", response);
-  } catch(error) {
-    console.log("Email not sent", error);
+    console.log("Email sent successfully");
+  } catch (error) {
+    console.error("Email error:", error);
   }
 };
-
 
 export default sendEmail;
