aa9bd658c12b5cbb6bdef12fa1e0267a7fecb7aa
Deepak <boradeepakreddy2005@gmail.com>
2026-03-03 11:43:23 +0530

    Fixed production port for Render

--- backend/server.js
+++ backend/server.js
@@ -14,7 +14,10 @@ connectDB();
 
 const app = express();
 
-app.use(cors());
+app.use(cors({
+  origin: "https://smart-expense-tracker-wine.vercel.app", 
+  credentials: true
+}));
 app.use(express.json());
 
 app.use("/api/auth", authRoutes);
@@ -26,6 +29,7 @@ app.get("/", (req, res) => {
   res.send("Smart Expense Tracker API Running...");
 });
 
-app.listen(5000, () => {
-  console.log("Server running on port 5000");
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
 });;
