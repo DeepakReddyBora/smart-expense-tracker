4a4f1156cbedc65a5f94e9bbf2edc0af788124d0
Deepak <boradeepakreddy2005@gmail.com>
2026-03-04 19:34:45 +0530

    Updated

--- backend/utils/sendEmail.js
+++ backend/utils/sendEmail.js
@@ -12,26 +12,31 @@ oAuth2Client.setCredentials({
 });
 
 export const sendEmail = async (to, otp) => {
-  const accessToken = await oAuth2Client.getAccessToken();
+  try {
+    const accessToken = await oAuth2Client.getAccessToken();
 
-  const transporter = nodemailer.createTransport({
-    service: "gmail",
-    auth: {
-      type: "OAuth2",
-      user: process.env.EMAIL,
-      clientId: process.env.CLIENT_ID,
-      clientSecret: process.env.CLIENT_SECRET,
-      refreshToken: process.env.REFRESH_TOKEN,
-      accessToken: accessToken.token,
-    },
-  });
+    const transporter = nodemailer.createTransport({
+      service: "gmail",
+      auth: {
+        type: "OAuth2",
+        user: process.env.EMAIL,
+        clientId: process.env.CLIENT_ID,
+        clientSecret: process.env.CLIENT_SECRET,
+        refreshToken: process.env.REFRESH_TOKEN,
+        accessToken: accessToken.token,
+      },
+    });
 
-  await transporter.sendMail({
-    from: process.env.EMAIL,
-    to,
-    subject: "Verify Your Account",
-    text: `Your OTP is ${otp}`,
-  });
+    await transporter.sendMail({
+      from: process.env.EMAIL,
+      to,
+      subject: "Verify Your Account",
+      text: `Your OTP is ${otp}`,
+    });
 
-  console.log("Email sent via Gmail");
+    console.log("Email sent via Gmail");
+
+  } catch (error) {
+    console.log("EMAIL ERROR:", error);
+  }
 };
